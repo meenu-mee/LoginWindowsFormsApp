bac38af0b6dab506ccdf11b03b24b427b6f90a83
Meenu <meenu.mee@outlook.com>
2020-09-18 22:27:13 +0530

    Added ToolBox

--- DemoApplication/DemoApplication/Form1.Designer.cs
+++ DemoApplication/DemoApplication/Form1.Designer.cs
@@ -28,13 +28,210 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("Label");
+            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("Button");
+            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("Checkbox");
+            System.Windows.Forms.TreeNode treeNode8 = new System.Windows.Forms.TreeNode("Root", new System.Windows.Forms.TreeNode[] {
+            treeNode5,
+            treeNode6,
+            treeNode7});
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox = new System.Windows.Forms.GroupBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstCity = new System.Windows.Forms.ListBox();
+            this.rdMale = new System.Windows.Forms.RadioButton();
+            this.rdFemale = new System.Windows.Forms.RadioButton();
+            this.chkC = new System.Windows.Forms.CheckBox();
+            this.chkASP = new System.Windows.Forms.CheckBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.imageASP = new System.Windows.Forms.PictureBox();
+            this.groupBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imageASP)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox
+            // 
+            this.groupBox.Controls.Add(this.txtAddress);
+            this.groupBox.Controls.Add(this.txtName);
+            this.groupBox.Controls.Add(this.label2);
+            this.groupBox.Controls.Add(this.label1);
+            this.groupBox.Location = new System.Drawing.Point(33, 25);
+            this.groupBox.Name = "groupBox";
+            this.groupBox.Size = new System.Drawing.Size(302, 126);
+            this.groupBox.TabIndex = 0;
+            this.groupBox.TabStop = false;
+            this.groupBox.Text = "User Details";
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(102, 75);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(164, 23);
+            this.txtAddress.TabIndex = 3;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(102, 36);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(164, 23);
+            this.txtName.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Address";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // lstCity
+            // 
+            this.lstCity.FormattingEnabled = true;
+            this.lstCity.ItemHeight = 15;
+            this.lstCity.Items.AddRange(new object[] {
+            "Pune",
+            "Mumbai",
+            "Bangalore"});
+            this.lstCity.Location = new System.Drawing.Point(179, 169);
+            this.lstCity.Name = "lstCity";
+            this.lstCity.Size = new System.Drawing.Size(120, 94);
+            this.lstCity.TabIndex = 4;
+            this.lstCity.SelectedIndexChanged += new System.EventHandler(this.lstCity_SelectedIndexChanged);
+            // 
+            // rdMale
+            // 
+            this.rdMale.AutoSize = true;
+            this.rdMale.Location = new System.Drawing.Point(354, 60);
+            this.rdMale.Name = "rdMale";
+            this.rdMale.Size = new System.Drawing.Size(51, 19);
+            this.rdMale.TabIndex = 5;
+            this.rdMale.TabStop = true;
+            this.rdMale.Text = "Male";
+            this.rdMale.UseVisualStyleBackColor = true;
+            // 
+            // rdFemale
+            // 
+            this.rdFemale.AutoSize = true;
+            this.rdFemale.Location = new System.Drawing.Point(354, 99);
+            this.rdFemale.Name = "rdFemale";
+            this.rdFemale.Size = new System.Drawing.Size(63, 19);
+            this.rdFemale.TabIndex = 6;
+            this.rdFemale.TabStop = true;
+            this.rdFemale.Text = "Female";
+            this.rdFemale.UseVisualStyleBackColor = true;
+            // 
+            // chkC
+            // 
+            this.chkC.AutoSize = true;
+            this.chkC.Location = new System.Drawing.Point(354, 151);
+            this.chkC.Name = "chkC";
+            this.chkC.Size = new System.Drawing.Size(41, 19);
+            this.chkC.TabIndex = 7;
+            this.chkC.Text = "C#";
+            this.chkC.UseVisualStyleBackColor = true;
+            // 
+            // chkASP
+            // 
+            this.chkASP.AutoSize = true;
+            this.chkASP.Location = new System.Drawing.Point(354, 176);
+            this.chkASP.Name = "chkASP";
+            this.chkASP.Size = new System.Drawing.Size(71, 19);
+            this.chkASP.TabIndex = 8;
+            this.chkASP.Text = "ASP.NET";
+            this.chkASP.UseVisualStyleBackColor = true;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(342, 240);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 9;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseMnemonic = false;
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(33, 169);
+            this.treeView1.Name = "treeView1";
+            treeNode5.Name = "Node1";
+            treeNode5.Text = "Label";
+            treeNode6.Name = "Node2";
+            treeNode6.Text = "Button";
+            treeNode7.Name = "Node3";
+            treeNode7.Text = "Checkbox";
+            treeNode8.Name = "Node0";
+            treeNode8.Text = "Root";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode8});
+            this.treeView1.Size = new System.Drawing.Size(121, 97);
+            this.treeView1.TabIndex = 10;
+            // 
+            // imageASP
+            // 
+            this.imageASP.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.imageASP.Image = ((System.Drawing.Image)(resources.GetObject("imageASP.Image")));
+            this.imageASP.Location = new System.Drawing.Point(441, 25);
+            this.imageASP.Name = "imageASP";
+            this.imageASP.Size = new System.Drawing.Size(402, 238);
+            this.imageASP.TabIndex = 11;
+            this.imageASP.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(960, 331);
+            this.Controls.Add(this.imageASP);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.chkASP);
+            this.Controls.Add(this.chkC);
+            this.Controls.Add(this.rdFemale);
+            this.Controls.Add(this.rdMale);
+            this.Controls.Add(this.groupBox);
+            this.Controls.Add(this.lstCity);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox.ResumeLayout(false);
+            this.groupBox.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imageASP)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lstCity;
+        private System.Windows.Forms.RadioButton rdMale;
+        private System.Windows.Forms.RadioButton rdFemale;
+        private System.Windows.Forms.CheckBox chkC;
+        private System.Windows.Forms.CheckBox chkASP;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.PictureBox imageASP;
     }
 }
 
--- DemoApplication/DemoApplication/Form1.cs
+++ DemoApplication/DemoApplication/Form1.cs
@@ -17,5 +17,20 @@ namespace DemoApplication
             InitializeComponent();
         }
 
+
+        private void lstCity_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string text = lstCity.GetItemText(lstCity.SelectedItem);
+            MessageBox.Show(text);
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            String name = txtName.Text;
+            String address = txtAddress.Text;
+
+            MessageBox.Show(name +", "+ address);
+        }
+
     }
 }
